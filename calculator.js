52a645696b9c4daa2c71a48fc02e983a403cfaad
sigtuple-varun-kumar <varunkumar@sigtuple.com>
2025-08-07 21:09:43 +0530

    Added production code for new line seperated expressions

--- calculator.js
+++ calculator.js
@@ -1,6 +1,6 @@
 export default class Calculator {
     add(expression) {
-        const numbers = expression.split(",");
+        const numbers = expression.split(/,|\n/g);
         let ans = 0;
         for (const number of numbers) {
             ans += parseInt(number || 0);
