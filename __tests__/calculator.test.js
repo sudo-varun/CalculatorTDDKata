34f7badc0f61e53099f587a806db84de5ee2bb7b
sigtuple-varun-kumar <varunkumar@sigtuple.com>
2025-08-07 21:05:50 +0530

    Refactor tests and add a edge case

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -1,6 +1,6 @@
 import Calculator from '../calculator.js';
 
-describe('Calculator', () => {
+describe('Basic functionality', () => {
     const calculator = new Calculator();
     test("empty string should return 0", () => {
         expect(calculator.add("")).toBe(0);
@@ -8,8 +8,16 @@ describe('Calculator', () => {
     test("Single number should return the number", () => {
         expect(calculator.add("7")).toBe(7);
     })
-    test("comma seperated numbers in expression should evaluate to addition of numbers", () => {
-        expect(calculator.add("4,2")).toBe(6);
-        expect(calculator.add("4,2,1")).toBe(7);
-    })
 });
+describe("Comma seperated expression", () => {
+    const calculator = new Calculator();
+    test("'1,2' should evaluate to 3", ()=> {
+        expect(calculator.add("1,2")).toBe(3);
+    })
+    test("'1,2,3,4' should evaluate to 10", () => {
+        expect(calculator.add("1,2,3,4")).toBe(10);
+    })
+    test("'1,' should evaluate to 1", () => {
+        expect(calculator.add("1,")).toBe(1);    
+    })
+})
