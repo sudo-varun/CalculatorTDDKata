b8c6beb4254d12c010283bf0f6c9a0e1bf51b488
sigtuple-varun-kumar <varunkumar@sigtuple.com>
2025-08-07 23:38:41 +0530

    Refactor test and add test case for larger numbers

---
+++ __tests__/advancedCalculator.test.js
@@ -0,0 +1,9 @@
+describe("Numbers greater than 1000", () => {
+    const calculator = new Calculator();
+    test("'1001,2' should evaluate to 2", () => {
+        expect(calculator.add("1001,2")).toBe(2);
+    });
+    test("'1000,2000' should evaluate to 1000", () => {
+        expect(calculator.add("1000,2000")).toBe(1000);
+    });
+});
