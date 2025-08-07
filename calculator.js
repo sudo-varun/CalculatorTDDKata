ea3f465b216685a246b52a53cee64dab52ba5893
sigtuple-varun-kumar <varunkumar@sigtuple.com>
2025-08-07 23:09:13 +0530

    Add code for custom delimiters

--- calculator.js
+++ calculator.js
@@ -1,10 +1,25 @@
 export default class Calculator {
+    defaultDelimiter = ',';
     add(expression) {
-        const numbers = expression.split(/,|\n/g);
+        if(this.hasCustomDelimiter(expression)) {
+            this.defaultDelimiter = this.getCustomDelimiter(expression);
+            expression = this.removeCustomDelimiter(expression);
+        }
+        const regex = new RegExp(`[${this.defaultDelimiter}\\n]`);
+        const numbers = expression.split(regex);
         let ans = 0;
         for (const number of numbers) {
             ans += parseInt(number || 0);
         }
         return ans;
+    }
+    hasCustomDelimiter(expression) {
+        return expression.startsWith('//');
+    }
+    getCustomDelimiter(expression) {
+        return expression.charAt(2);
+    }
+    removeCustomDelimiter(expression) {
+        return expression.substring(expression.indexOf('\n') + 1);
     }
 }
